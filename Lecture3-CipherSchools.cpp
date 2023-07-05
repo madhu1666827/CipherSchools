b8d4f055438058361b32beff4733ac7b42f414d3
madhu1666827 <122732372+madhu1666827@users.noreply.github.com>
2023-07-05 13:02:14 +0530

    Add files via upload

---
+++ Lecture3-CipherSchools.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//declaring 2 variables and assigning values for the addition purpose
+	int a,b;
+	a=3;
+	b=5;
+	int c;
+	c=a+b;
+	
+	cout<<c<<endl;
+	cout<<"a+b";
+	
+}
