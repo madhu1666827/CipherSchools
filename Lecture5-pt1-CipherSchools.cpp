1c17edfe9309b47940a547d69aca91b4392ae842
madhu1666827 <122732372+madhu1666827@users.noreply.github.com>
2023-07-06 15:55:14 +0530

    Add files via upload

---
+++ Lecture5-pt1-CipherSchools.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	// Lecture 5 initializing arrays and working on arrays
+	int a[5];
+	// the indexing of arrays will starts from 0 not from 1 for example:
+	// a[0],a[1],a[2]..... and so on
+	//the elements in an array are of same type
+	
+	
+	
+	
+	//If there are no arrays we need to declare multiple variables like below to display the numbers in reverse order
+	
+	int b,c,d,e,f;
+	cout<<"Enyter the values of five variables"<<endl;
+	
+	cin>>b>>c>>d>>e>>f;	
+	
+	cout<<"Reverse order is"<<endl;
+	cout<<f<<e<<d<<c<<b;
+}
